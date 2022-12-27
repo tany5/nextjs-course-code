95629b542e6573fd2862cde046f7588748e1c078
tany5 <tanmay1dey@gmail.com>
2022-12-27 19:29:04 +0530

    [WIP] Working on nextjs course code

---
+++ pages/events/[eventid]/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FilteredEventPage() {
+  return (
+    <div>
+      <h1>Events Slug Details</h1>
+    </div>
+  );
+}
+
+export default FilteredEventPage;
---
+++ pages/events/[eventid]/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EventsDetails() {
+  return (
+    <div>
+      <h1>Events Details</h1>
+    </div>
+  );
+}
+
+export default EventsDetails;
